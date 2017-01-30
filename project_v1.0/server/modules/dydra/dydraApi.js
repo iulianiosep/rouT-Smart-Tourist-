6ebe66ecf053501ab764bdd9648f495c34ff0519
acatrinei.cornel.paul@gmail.com <Thebestishere18>
2017-01-31 01:44:00 +0200

    Dydra Intern API updated to version 1.1.0 (error responses was added)

--- project_v1.0/server/modules/dydra/dydraApi.js
+++ project_v1.0/server/modules/dydra/dydraApi.js
@@ -13,63 +13,333 @@ var
     MODULE = 'LOCATION',
     router = express.Router();
 
-
-router.get('/api/select', function(req, res, next){
+router.post('/api/persons/addNewPerson', function(req, res, next){
 
     client = new sparql.Client(config.DYDRA_ENDPOINT);
-    queryString = 'SELECT ?name ?nick WHERE  { ?x foaf:name ?name . ?x foaf:nick ?nick .}';
+    personName = req.body.personName;
+    personLink =    '<' + config.DYDRA_ENDPOINT + '/personsRoot/' + personName + '>';
 
-    var myCallback = function (data) {
+    queryCheck = 'ASK { ' +  ' ?x rdf:type foaf:Person .' +
+            ' ?x foaf:name "' + personName + '" }';
+
+    queryString = 'INSERT DATA { ' + personLink + ' foaf:name "' + personName +'";' +
+        ' rdf:type foaf:Person . }';
+
+    console.log(queryCheck);
+    console.log(queryString);
+
+    var sendResponse201 = function () {
+        res.status(201);
         res.send(JSON.stringify({
-            data:data
+            person: personLink
         }));
     };
-    client.query(queryString, function(err, res) {
+    var sendResponse409 = function () {
+        res.status(409);
+        res.send(JSON.stringify({
+            reason: 'A Person with given name already exists'
+        }));
+    };
+
+    client.query(queryCheck, function(err, queryResult) {
         if (err) {
             console.log(err);
         } else {
-            myCallback(res.results.bindings);
+            if (queryResult.boolean == true){
+                sendResponse409();
+            }
+            else{
+                client.query(queryString, function(err) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        sendResponse201();
+                    }
+                });
+            }
         }
     });
 });
 
-router.get('/api/insert', function(req, res, next){
+router.post('/api/persons/:personName/addInterest', function(req, res, next){
 
     client = new sparql.Client(config.DYDRA_ENDPOINT);
-    queryString = 'INSERT DATA { ' + '<' + 'example/users/NodeJsUser4' + '>' + ' foaf:name'
-        + ' "NodeJsUser4"' + '; foaf:nick' + ' "NickJs4"' + '.}';
+    personName = req.params['personName'];
+    interestName = req.body.interestName;
+    personLink =    '<' + config.DYDRA_ENDPOINT + '/personsRoot/' + personName + '>';
+
+    queryCheckPerson = 'ASK { ' +  ' ?x rdf:type foaf:Person .' +
+        ' ?x foaf:name "' + personName + '" }';
+
+    queryCheckInterest = 'ASK {  ?x rdf:type foaf:Person . ' +
+        ' ?x foaf:name "' + personName + '" . ' +
+        ' ?x foaf:interest "' + interestName + '" }';
+
+    queryString = 'INSERT DATA { ' + personLink + ' foaf:interest "'+ interestName +'" }';
+
+    console.log(queryCheckInterest);
+
+    var sendResponse201 = function () {
+        res.status(201);
+        res.send(JSON.stringify({
+            person: personLink
+        }));
+    };
+
+    var sendResponse404 = function () {
+        res.status(404);
+        res.send(JSON.stringify({
+            reason: 'Person with the given name does not exists'
+        }));
+    };
+
+    var sendResponse409 = function () {
+        res.status(409);
+        res.send(JSON.stringify({
+            reason: 'The specified Person have already the specified interest'
+        }));
+    };
+
+
+    client.query(queryCheckPerson, function(err, queryResult) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (queryResult.boolean == false){
+                sendResponse404();
+            }
+            else{
+                client.query(queryCheckInterest, function(err, queryResult) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        if (queryResult.boolean == true){
+                            sendResponse409();
+                        } else {
+                            client.query(queryString, function(err){
+                                if(err){
+                                    console.log(err);
+                                } else {
+                                    sendResponse201();
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        }
+    });
+
+});
+
+router.post('/api/groups/addNewGroup', function(req, res, next){
+
+    client = new sparql.Client(config.DYDRA_ENDPOINT);
+    groupName = req.body.groupName;
+    groupLink = '<' + config.DYDRA_ENDPOINT + '/groupsRoot/' + groupName + '>';
+
+    queryCheckGroup = 'ASK {  ?x rdf:type foaf:Group . ?x foaf:name "' + groupName + '" }';
+
+    queryString = 'INSERT DATA { ' + groupLink +
+        ' foaf:name "'+ groupName +'";' +
+        ' foaf:focus "Travelling";' +
+        ' rdf:type foaf:Group.' +
+        ' }';
+
+    console.log(queryCheckGroup);
+    console.log(queryString);
+
+    var sendResponse201 = function () {
+        res.status(201);
+        res.send(JSON.stringify({
+            group: groupLink
+        }));
+    };
+    var sendResponse409 = function () {
+        res.status(409);
+        res.send(JSON.stringify({
+            reason: 'A Group with given name already exists'
+        }));
+    };
+
+    client.query(queryCheckGroup, function(err, queryResult) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (queryResult.boolean == true){
+                sendResponse409();
+            }
+            else{
+                client.query(queryString, function(err) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        sendResponse201();
+                    }
+                });
+            }
+        }
+    });
+});
+
+router.post('/api/groups/:groupName/addPerson', function(req, res, next) {
+
+    client = new sparql.Client(config.DYDRA_ENDPOINT);
+    groupName = req.params['groupName'];
+    personName = req.body.personName;
+    groupLink = '<' + config.DYDRA_ENDPOINT + '/groupsRoot/' + groupName + '>';
+    personLink = '<' + config.DYDRA_ENDPOINT + '/personsRoot/' + personName + '>';
+
+    queryCheckPerson = 'ASK { ' +  ' ?x rdf:type foaf:Person .' +
+        ' ?x foaf:name "' + personName + '" }';
+    queryCheckMembership = 'ASK {  ?x rdf:type foaf:Person . ' +
+        ' ?x foaf:name "' + personName + '" .' +
+        groupLink + ' foaf:member ?x }';
+    queryCheckGroup = 'ASK {  ?x rdf:type foaf:Group . ?x foaf:name "' + groupName + '" }';
+    queryString = 'INSERT DATA { ' + groupLink + ' foaf:member ' + personLink + '. }';
 
     console.log(queryString);
 
-    var myCallback = function (data) {
+    var sendResponse201 = function () {
+        res.status(201);
         res.send(JSON.stringify({
-            data:data
+            person: personLink
         }));
     };
-    client.query(queryString, function(err, res) {
+
+    var sendResponse404 = function (resourceName) {
+        res.status(404);
+        res.send(JSON.stringify({
+            reason: resourceName +  ' with the given name does not exists'
+        }));
+    };
+
+    var sendResponse409 = function () {
+        res.status(409);
+        res.send(JSON.stringify({
+            reason: 'A Person with given name is already a member of the specified Group'
+        }));
+    };
+
+    client.query(queryCheckGroup, function (err, queryResult) {
         if (err) {
             console.log(err);
         } else {
-            myCallback(res.status, res);
+            if (queryResult.boolean == true) {
+                client.query(queryCheckPerson, function(err, queryResult){
+                    if(err){
+                        console.log(err);
+                    } else {
+                        if(queryResult.boolean == false){
+                            sendResponse404('Person');
+                        } else {
+                            client.query(queryCheckMembership, function (err, queryResult) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    if (queryResult.boolean == true) {
+                                        sendResponse409();
+                                    }
+                                    else {
+                                        client.query(queryString, function (err) {
+                                            if (err) {
+                                                console.log(err);
+                                            } else {
+                                                sendResponse201();
+                                            }
+                                        });
+                                    }
+                                }
+                            });
+                        }
+                    }
+                })
+            }
+            else {
+                sendResponse404('Group');
+            }
         }
     });
 });
 
-router.get('/test', function (req, res, next) {
-    res.send(JSON.stringify({
-        data: {
-            bunica: 'ok'
+router.post('/api/groups/:groupName/addNewPerson', function(req, res, next){
+    client = new sparql.Client(config.DYDRA_ENDPOINT);
+    groupName = req.params['groupName'];
+    personName = req.body.personName;
+    groupLink = '<' + config.DYDRA_ENDPOINT + '/groupsRoot/' + groupName + '>';
+    personLink = '<' + config.DYDRA_ENDPOINT + '/personsRoot/' + personName + '>';
+
+    queryCheckPerson = 'ASK { ' +  ' ?x rdf:type foaf:Person .' +
+        ' ?x foaf:name "' + personName + '" }';
+    queryCheckMembership = 'ASK {  ?x rdf:type foaf:Person . ' +
+        ' ?x foaf:name "' + personName + '" .' +
+        groupLink + ' foaf:member ?x }';
+    queryCheckGroup = 'ASK {  ?x rdf:type foaf:Group . ?x foaf:name "' + groupName + '" }';
+    queryString = 'INSERT DATA { ' + groupLink + ' foaf:member ' + personLink + ' .  ' +
+        personLink + ' foaf:name "' + personName +'" . ' +
+        personLink + ' rdf:type foaf:Person  . }';
+
+    console.log(queryString);
+
+    var sendResponse201 = function () {
+        res.status(201);
+        res.send(JSON.stringify({
+            person: personLink
+        }));
+    };
+
+    var sendResponse404 = function (resourceName) {
+        res.status(404);
+        res.send(JSON.stringify({
+            reason: resourceName +  ' with the given name does not exists'
+        }));
+    };
+
+    var sendResponse409 = function () {
+        res.status(409);
+        res.send(JSON.stringify({
+            reason: 'A Person with given name already exists'
+        }));
+    };
+
+    client.query(queryCheckGroup, function (err, queryResult) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (queryResult.boolean == true) {
+                client.query(queryCheckPerson, function(err, queryResult){
+                    if(err){
+                        console.log(err);
+                    } else {
+                        if(queryResult.boolean == true){
+                            sendResponse409();
+                        } else {
+                            client.query(queryString, function (err) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    sendResponse201();
+                                }
+                            });
+                        }
+                    }
+                });
+            } else {
+                sendResponse404('Group');
+            }
         }
-    }))
-
+    })
 });
-
 
 router.get('/api/groups/:groupName/members', function(req, res, next){
 
     client = new sparql.Client(config.DYDRA_ENDPOINT);
     groupName = req.params['groupName'];
     groupLink = '<' + config.DYDRA_ENDPOINT + '/groupsRoot/' + groupName + '>'
+
+    queryCheckGroup = 'ASK {  ?x rdf:type foaf:Group . ?x foaf:name "' + groupName + '" }';
+
+    queryCheckGroup = 'ASK {  ?x rdf:type foaf:Group . ?x foaf:name "' + groupName + '" }';
 
     queryString = 'SELECT ?member ' +
         'WHERE { ' + groupLink +
@@ -79,29 +349,56 @@ router.get('/api/groups/:groupName/members', function(req, res, next){
 
     console.log(queryString);
 
-    var myCallback = function (data) {
+    var sendResponse200 = function (data) {
         members = [];
         data.forEach(function(element){
             members.push(element.member.value);
         });
+        res.status(200);
         res.send(JSON.stringify({
-            data:members
+            data: members
         }));
     };
-    client.query(queryString, function(err, res) {
+
+    var sendResponse404 = function () {
+        res.status(404);
+        res.send(JSON.stringify({
+            reason: 'Group Not found'
+        }));
+    };
+
+
+    client.query(queryCheckGroup, function(err, queryResult) {
         if (err) {
             console.log(err);
         } else {
-            myCallback(res.results.bindings);
+            if (queryResult.boolean == false){
+                sendResponse404();
+            }
+            else{
+                client.query(queryString, function(err, res) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        sendResponse200(res.results.bindings);
+                    }
+                });
+            }
         }
     });
+
+
+
 });
+
 
 router.get('/api/groups/:groupName/interests', function(req, res, next){
 
     client = new sparql.Client(config.DYDRA_ENDPOINT);
     groupName = req.params['groupName'];
     groupLink = '<' + config.DYDRA_ENDPOINT + '/groupsRoot/' + groupName + '>'
+
+    queryCheckGroup = 'ASK {  ?x rdf:type foaf:Group . ?x foaf:name "' + groupName + '" }';
 
     queryString = 'SELECT ?interest' +
         ' WHERE { ' + groupLink +
@@ -112,163 +409,43 @@ router.get('/api/groups/:groupName/interests', function(req, res, next){
 
     console.log(queryString);
 
-    var myCallback = function (data) {
+    var sendResponse200 = function (data) {
        interests = [];
        data.forEach(function(element){
        interests.push(element.interest.value);
         });
+       res.status(200);
         res.send(JSON.stringify({
             data: interests
         }));
     };
-    client.query(queryString, function(err, res) {
-        if (err) {
-            console.log(err);
-        } else {
-            myCallback(res.results.bindings);
-        }
-    });
-});
 
-
-router.post('/api/groups/addNewGroup', function(req, res, next){
-
-    client = new sparql.Client(config.DYDRA_ENDPOINT);
-    groupName = req.body.groupName;
-    groupLink = '<' + config.DYDRA_ENDPOINT + '/groupsRoot/' + groupName + '>';
-
-    queryString = 'INSERT DATA { ' + groupLink +
-        ' foaf:name "'+ groupName +'";' +
-        ' foaf:focus "Travelling";' +
-        ' rdf:type foaf:Group.' +
-        ' }';
-
-    console.log(req);
-    console.log(queryString);
-
-    var myCallback = function () {
-        res.status(201);
+    var sendResponse404 = function () {
+        res.status(404);
         res.send(JSON.stringify({
-            group: groupLink
+            reason: 'Group Not found'
         }));
     };
-    client.query(queryString, function(err, queryResult) {
+
+    client.query(queryCheckGroup, function(err, queryResult) {
         if (err) {
             console.log(err);
         } else {
-            myCallback();
+            if (queryResult.boolean == false){
+                sendResponse404();
+            }
+            else{
+                client.query(queryString, function(err, res) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        sendResponse200(res.results.bindings);
+                    }
+                });
+            }
         }
     });
-});
 
-router.post('/api/groups/:groupName/addPerson', function(req, res, next){
-
-    client = new sparql.Client(config.DYDRA_ENDPOINT);
-    groupName = req.params['groupName'];
-    personName = req.body.personName;
-    groupLink =     '<' + config.DYDRA_ENDPOINT + '/groupsRoot/' + groupName + '>';
-    personLink =    '<' + config.DYDRA_ENDPOINT + '/personsRoot/' + personName + '>';
-
-    queryString = 'INSERT DATA { ' + groupLink + ' foaf:member ' + personLink + '. }';
-
-    console.log(queryString);
-
-    var myCallback = function () {
-        res.status(201);
-        res.send(JSON.stringify({
-            group: groupLink
-        }));
-    };
-    client.query(queryString, function(err, queryResult) {
-        if (err) {
-            console.log(err);
-        } else {
-            myCallback();
-        }
-    });
-});
-
-router.post('/api/persons/addNewPerson', function(req, res, next){
-
-    client = new sparql.Client(config.DYDRA_ENDPOINT);
-    personName = req.body.personName;
-    personLink =    '<' + config.DYDRA_ENDPOINT + '/personsRoot/' + personName + '>';
-
-    queryString = 'INSERT DATA { ' + personLink + ' foaf:name "' + personName +'";' +
-        ' rdf:type foaf:Person . }';
-
-    console.log(queryString);
-
-    var myCallback = function () {
-        res.status(201);
-        res.send(JSON.stringify({
-            person: personLink
-        }));
-    };
-    client.query(queryString, function(err, queryResult) {
-        if (err) {
-            console.log(err);
-        } else {
-            myCallback();
-        }
-    });
-});
-
-
-router.post('/api/groups/:groupName/addNewPerson', function(req, res, next){
-
-    client = new sparql.Client(config.DYDRA_ENDPOINT);
-    personName = req.body.personName;
-    groupName = req.params['groupName'];
-    personLink =    '<' + config.DYDRA_ENDPOINT + '/personsRoot/' + personName + '>';
-    groupLink =     '<' + config.DYDRA_ENDPOINT + '/groupsRoot/' + groupName + '>';
-
-    queryString = 'INSERT DATA { ' + personLink + ' foaf:name "' + personName +'";' +
-        ' rdf:type foaf:Person . ' +
-        groupLink + ' foaf:member ' + personLink  + ' }';
-
-    console.log(queryString);
-
-    var myCallback = function () {
-        res.status(201);
-        res.send(JSON.stringify({
-            person: personLink
-        }));
-    };
-    client.query(queryString, function(err, queryResult) {
-        if (err) {
-            console.log(err);
-        } else {
-            myCallback();
-        }
-    });
-});
-
-
-router.post('/api/persons/:personName/addInterest', function(req, res, next){
-
-    client = new sparql.Client(config.DYDRA_ENDPOINT);
-    personName = req.params['personName'];
-    interestName = req.body.interestName;
-    personLink =    '<' + config.DYDRA_ENDPOINT + '/personsRoot/' + personName + '>';
-
-    queryString = 'INSERT DATA { ' + personLink + ' foaf:interest "'+ interestName +'" }';
-
-    console.log(queryString);
-
-    var myCallback = function () {
-        res.status(201);
-        res.send(JSON.stringify({
-            person: personLink
-        }));
-    };
-    client.query(queryString, function(err, queryResult) {
-        if (err) {
-            console.log(err);
-        } else {
-            myCallback();
-        }
-    });
 });
 
 
