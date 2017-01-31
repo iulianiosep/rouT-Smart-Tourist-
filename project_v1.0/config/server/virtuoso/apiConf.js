36597228f94e90d098d75b78162741eb8e07473b
iulianiosep <iosep.iulian@yahoo.com>
2017-01-31 21:10:14 +0200

    Quries nmecessary for take information from DbPedia

--- project_v1.0/config/server/virtuoso/apiConf.js
+++ project_v1.0/config/server/virtuoso/apiConf.js
@@ -1,6 +1,7 @@
 var CONFIG = {
     CLIENT_ENDPOINT : 'http://localhost:8890/sparql',
-    SPARQL_ENDPOINT : 'http://linkedgeodata.org/sparql'
+    SPARQL_ENDPOINT : "http://dbpedia.org/sparql",
+    SPARQL_ENDPOINT_ALTERNATIVE : 'http://linkedgeodata.org/sparql'
 };
 
 module.exports = CONFIG;
